c9672bed238d284fb39a67567fd67af0cf2360d1
Alex Gerdes <alexg@chalmers.se>
2025-07-07 16:39:27 +0200

    Choosing color schemes for nvim

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -33,8 +33,10 @@ return {
   {
     "f-person/auto-dark-mode.nvim",
     opts = {
-      set_dark_mode = function() vim.api.nvim_set_option_value("background", "dark", {}) end,
-      set_light_mode = function() vim.api.nvim_set_option_value("background", "light", {}) end,
+      -- set_dark_mode = function() vim.api.nvim_set_option_value("background", "dark", {}) end,
+      -- set_light_mode = function() vim.api.nvim_set_option_value("background", "light", {}) end,
+      set_dark_mode = function() vim.cmd [[colorscheme catppuccin-mocha]] end,
+      set_light_mode = function() vim.cmd [[colorscheme astrolight]] end,
       update_interval = 3000,
       fallback = "dark",
     },
@@ -71,5 +73,10 @@ return {
       --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
     },
+  },
+
+  -- Tinted colorscheme for base16 and base24 themes
+  {
+    "tinted-theming/tinted-vim",
   },
 }
