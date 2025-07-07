73a487011cc66451ce5ef44258298bea5edbf50c
Alex Gerdes <alexg@chalmers.se>
2025-07-07 10:50:07 +0200

    Added basic vim settings to AstroNvim

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -1,5 +1,23 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+local opt = vim.opt
+
+-- Basic settings
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.shiftround = false
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+opt.copyindent = true
+opt.shell = "/bin/zsh"
+opt.cursorline = true
+opt.encoding = "utf-8"
+opt.incsearch = true -- Starts search before enter
+opt.autoread = true -- Reads files again if they have been changed outside of vim
+
+-- Recognize docker files
+vim.filetype.add { extension = { docker = "dockerfile" } }
