93f1f1afaf7f1d60d0d65d7bc55aa01dcd52350a
Alex Gerdes <alexg@chalmers.se>
2025-07-11 10:27:04 +0200

    Don't configure colorscheme in vscode

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -23,64 +23,66 @@ opt.autoread = true -- Reads files again if they have been changed outside of vi
 vim.filetype.add { extension = { docker = "dockerfile" } }
 
 -- Configure the flexoki colorscheme
-require("flexoki").setup {
-  variant = "dawn", -- auto, moon, or dawn
-  dim_inactive_windows = true,
-  extend_background_behind_borders = true,
+if not vim.g.vscode then
+  require("flexoki").setup {
+    variant = "dawn", -- auto, moon, or dawn
+    dim_inactive_windows = true,
+    extend_background_behind_borders = true,
 
-  enable = {
-    terminal = true,
-  },
+    enable = {
+      terminal = true,
+    },
 
-  styles = {
-    bold = true,
-    italic = false,
-  },
+    styles = {
+      bold = true,
+      italic = false,
+    },
 
-  groups = {
-    border = "muted",
-    link = "purple_two",
-    panel = "surface",
+    groups = {
+      border = "muted",
+      link = "purple_two",
+      panel = "surface",
 
-    error = "red_one",
-    hint = "purple_one",
-    info = "cyan_one",
-    ok = "green_one",
-    warn = "orange_one",
-    note = "blue_one",
-    todo = "magenta_one",
+      error = "red_one",
+      hint = "purple_one",
+      info = "cyan_one",
+      ok = "green_one",
+      warn = "orange_one",
+      note = "blue_one",
+      todo = "magenta_one",
 
-    git_add = "green_one",
-    git_change = "yellow_one",
-    git_delete = "red_one",
-    git_dirty = "yellow_one",
-    git_ignore = "muted",
-    git_merge = "purple_one",
-    git_rename = "blue_one",
-    git_stage = "purple_one",
-    git_text = "magenta_one",
-    git_untracked = "subtle",
+      git_add = "green_one",
+      git_change = "yellow_one",
+      git_delete = "red_one",
+      git_dirty = "yellow_one",
+      git_ignore = "muted",
+      git_merge = "purple_one",
+      git_rename = "blue_one",
+      git_stage = "purple_one",
+      git_text = "magenta_one",
+      git_untracked = "subtle",
 
-    h1 = "purple_two",
-    h2 = "cyan_two",
-    h3 = "magenta_two",
-    h4 = "orange_two",
-    h5 = "blue_two",
-    h6 = "cyan_two",
-  },
+      h1 = "purple_two",
+      h2 = "cyan_two",
+      h3 = "magenta_two",
+      h4 = "orange_two",
+      h5 = "blue_two",
+      h6 = "cyan_two",
+    },
 
-  palette = {
-    -- Override the builtin palette per variant
-    -- dawn = {
-    --   _nc = "#efefef",
-    --   base = "#f9f9f9",
-    --   surface = "#fafafa",
-    --   overlay = "#efefef",
-    -- },
-  },
+    palette = {
+      -- Override the builtin palette per variant
+      -- dawn = {
+      --   _nc = "#efefef",
+      --   base = "#f9f9f9",
+      --   surface = "#fafafa",
+      --   overlay = "#efefef",
+      -- },
+    },
 
-  highlight_groups = {
-    -- Comment = { fg = "subtle" },
-    -- VertSplit = { fg = "muted", bg = "muted" },
-  },
-}
+    highlight_groups = {
+      -- Comment = { fg = "subtle" },
+      -- VertSplit = { fg = "muted", bg = "muted" },
+    },
+  }
+end
