6bf4fcf970dba11552f45c316056740dda307f80
Alex Gerdes <agerdes@me.com>
2026-03-19 15:55:40 +0100

    Fix Treesitter LaTeX parser install with newer tree-sitter CLI

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,27 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  config = function(_, opts)
+    local install = require "nvim-treesitter.install"
+    local ts_utils = require "nvim-treesitter.utils"
+    local version = ts_utils.ts_cli_version()
+
+    if version then
+      local major, minor = version:match "^(%d+)%.(%d+)"
+      major = tonumber(major)
+      minor = tonumber(minor)
+
+      if major and minor then
+        if major > 0 or minor >= 25 then
+          install.ts_generate_args = { "generate", "--abi", tostring(vim.treesitter.language_version) }
+        else
+          install.ts_generate_args = { "generate", "--no-bindings", "--abi", tostring(vim.treesitter.language_version) }
+        end
+      end
+    end
+
+    require("nvim-treesitter.configs").setup(opts)
+  end,
   opts = {
     ensure_installed = {
       "lua",
