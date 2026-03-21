7b69010b1776da316491d08101682f68ef078bae
Alex Gerdes <agerdes@me.com>
2026-03-21 19:40:59 +0100

    Changed yaziline to blocks

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,7 +1,9 @@
 require("githead"):setup()
 require("full-border"):setup()
-require("yaziline"):setup()
-require("git"):setup {
+require("yaziline"):setup({
+	separator_style = "liney",
+})
+require("git"):setup({
 	-- Order of status signs showing in the linemode
 	order = 1500,
-}
+})
