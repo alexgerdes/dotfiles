1a414733039d3ae895da7b57566e94ec1bf13df4
Alex Gerdes <agerdes@me.com>
2026-03-23 14:17:36 +0100

    Fixing yazi colors

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,6 +1,12 @@
 require("githead"):setup()
 require("full-border"):setup()
 require("yaziline"):setup({
+	color = "#89b4fa",
+	secondary_color = "#6c7086",
+	default_files_color = "#6c7086", -- color of the file counter when it's inactive
+	selected_files_color = "#cdd6f4",
+	yanked_files_color = "#94e2d5",
+	cut_files_color = "#f38ba8",
 	separator_style = "liney",
 })
 require("git"):setup({
