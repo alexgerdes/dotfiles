f28568e024ff9759d5f7b25e5c87b08fb1cce531
Alex Gerdes <alexg@chalmers.se>
2026-06-08 09:40:03 +0200

    use a dedicated treesitter manager.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,57 +1,45 @@
--- Customize Treesitter
-
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function(_, opts)
-    local install = require "nvim-treesitter.install"
-    local ts_utils = require "nvim-treesitter.utils"
-    local version = ts_utils.ts_cli_version()
-
-    if version then
-      local major, minor = version:match "^(%d+)%.(%d+)"
-      major = tonumber(major)
-      minor = tonumber(minor)
-
-      if major and minor then
-        if major > 0 or minor >= 25 then
-          install.ts_generate_args = { "generate", "--abi", tostring(vim.treesitter.language_version) }
-        else
-          install.ts_generate_args = { "generate", "--no-bindings", "--abi", tostring(vim.treesitter.language_version) }
-        end
-      end
-    end
-
-    require("nvim-treesitter.configs").setup(opts)
+  "romus204/tree-sitter-manager.nvim",
+  dependencies = {}, -- tree-sitter CLI must be installed system-wide
+  config = function()
+    require("tree-sitter-manager").setup {
+      -- Default Options
+      -- ensure_installed = {}, -- list of parsers to install at the start of a neovim session
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        -- Web
+        "html",
+        "css",
+        "javascript",
+        "json",
+        "svelte",
+        "typescript",
+        -- Main
+        "haskell",
+        "java",
+        "python",
+        "rust",
+        "erlang",
+        "elixir",
+        "ocaml",
+        -- Low level
+        "c",
+        "llvm",
+        "make",
+        "nasm",
+        -- Other
+        "dockerfile",
+        -- add more arguments for adding more treesitter parsers
+      },
+      -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+      -- auto_install = false, -- if enabled, install missing parsers when editing a new file
+      -- highlight = true, -- treesitter highlighting is enabled by default
+      -- languages = {}, -- override or add new parser sources
+      -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+      -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+    }
   end,
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "vimdoc",
-      -- Web
-      "html",
-      "css",
-      "javascript",
-      "json",
-      "svelte",
-      "typescript",
-      -- Main
-      "haskell",
-      "java",
-      "python",
-      "rust",
-      "erlang",
-      "elixir",
-      "ocaml",
-      -- Low level
-      "c",
-      "llvm",
-      "make",
-      "nasm",
-      -- Other
-      "dockerfile",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
 }
